b00bce8cc5ae986b3d6b73a3fc3d7d5e07d58b31
Lihle Mqhayi <2018293130@ufs4life.ac.za>
2023-04-11 22:37:00 +0200

    Add project files.

---
+++ SumOfFourthPower/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    //Explanation:
+
+    //The program loops through all four-digit numbers from 1000 to 9999 using a for loop.For each number, it calculates the sum of the fourth powers of its digits by extracting each digit one by one using a while loop and then using the Math.Pow() function to raise it to the fourth power and add it to the sum.
+
+    //If the sum is equal to the original number, the program prints the number to the console.
+
+   //Note that this program assumes that there are exactly three four-digit numbers that can be written as the sum of the fourth powers of their digits. This is known to be true, but if you wanted to make the program more general, you could add a counter variable that keeps track of how many such numbers have been found and stops the loop once it reaches three.
+    static void Main(string[] args)
+    {
+        for (int i = 1000; i <= 9999; i++)
+        {
+            int sum = 0;
+            int temp = i;
+
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += (int)Math.Pow(digit, 4);
+                temp /= 10;
+            }
+
+            if (sum == i)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
